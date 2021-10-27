5f8bd27bee50ef7fa61f7950016e3fd8b82ea6ce
Brendan Downing <b.downing.code@gmail.com>
2021-10-27 15:21:11 -0400

    Add files via upload

---
+++ JavaCore/task11/task1123/Solution.java
@@ -0,0 +1,45 @@
+package com.codegym.task.task11.task1123;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair<Integer, Integer> result = getMinimumAndMaximum(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The maximum is " + result.y);
+    }
+
+    public static Pair<Integer, Integer> getMinimumAndMaximum(int[] array) {
+        if (array == null || array.length == 0) {
+            return new Pair<Integer, Integer>(null, null);
+        }
+        int minimum = array[0];
+        int maximum = array[0];
+
+        for (int i : array){
+            if (i > maximum) {
+                maximum = i;
+            }
+            if (i < minimum) {
+                minimum = i;
+            }
+
+        }
+
+        return new Pair<Integer, Integer>(minimum, maximum);
+    }
+
+
+    public static class Pair<X, Y> {
+        public X x;
+        public Y y;
+
+        public Pair(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
