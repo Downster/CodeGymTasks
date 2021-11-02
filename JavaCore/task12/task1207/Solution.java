5aead5ea61613a1cdee7afa6e012e063cf5050b4
Brendan Downing <b.downing.code@gmail.com>
2021-11-02 11:47:12 -0600

    Add files via upload

---
+++ JavaCore/task12/task1207/Solution.java
@@ -0,0 +1,24 @@
+package com.codegym.task.task12.task1207;
+
+/* 
+int and Integer
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 2;
+        print(a);
+        Integer b = 4;
+        print(b);
+
+
+    }
+
+    static void print (int a) {
+
+    }
+    static void print (Integer b){
+
+    }
+}
---
+++ JavaCore/task12/task1208/Solution.java
@@ -0,0 +1,28 @@
+package com.codegym.task.task12.task1208;
+
+/* 
+Freedom of the press
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static void print(float b){
+
+    }
+    static void print(int a){
+
+    }
+    static void print(double c){
+
+    }
+    static void print(byte a){
+
+    }
+    static void print(Integer c){
+
+    }
+}
