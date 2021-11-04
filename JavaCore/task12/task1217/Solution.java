36467bb0ebe5879e414c599ec8f4c9b640b980fb
Brendan Downing <b.downing.code@gmail.com>
2021-11-04 13:39:01 -0600

    Add files via upload

---
+++ JavaCore/task12/task1217/Solution.java
@@ -0,0 +1,25 @@
+package com.codegym.task.task12.task1217;
+
+/* 
+Fly, run and swim
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        void fly(String plane);
+    }
+
+    public interface CanRun {
+        void run(String human);
+    }
+
+    public interface CanSwim {
+        void swim(String human);
+    }
+
+}
